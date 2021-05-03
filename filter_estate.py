558de2f389e8fefb1f17c99e7923b702978bc588
Diego Yance <diegoand2009@hotmail.com>
2021-05-03 08:55:35 -0500

    Added the respective documentation on task 1 and 2

--- filter_estate.py
+++ filter_estate.py
@@ -1,11 +1,13 @@
 from datetime import date
 
 def zoneA(place):
+    """ Method to calculate the price of properties which are in zone A """
     year = date.today().year
     price = (place['metros']*1000 + place['habitaciones']*5000 + place['garaje']*15000) * (1 - (year-place['año'])/100)
     return place | {'precio': round(price, 2)}
 
 def zoneB(place):
+    """ Method to calculate the price of properties which are in zone B """
     year = date.today().year
     price = (place['metros']*1000 + place['habitaciones']*5000 + place['garaje']*15000) * (1 - (year-place['año'])/100) *1.5
     return place | {'precio': round(price, 2)}
@@ -20,17 +22,23 @@ def run():
     
     budget = input("Enter the budget for the property purchase \n")
     
+    #Se valida si el presupuesto ingresado es un número y también si es mayor que cero
     try:
         budget = float(budget)
         if budget<0: raise ValueError
     except ValueError:
         return print('Enter only numbers greater than 0')
 
+    #Se realiza el calculo del precio de los inmuebles acorde a la zona que pertenezcan mediante un previo filtro, una vez hecho esto
+    #se llama a los metodos zoneA y zoneB que se encargan de retornar el mismo diccionario junto a una nueva llave llamada precio
+    #que tendrá el valor del mismo. Si bien es cierto que lo único que difieren los precios de la zonaB con la zonaA es un factor multiplicativo de 1.5
+    #por temas de mantenimiento de código en caso de que estas fórmulas cambien y no dependan de la otra, es mejor tener la forma de sacar su precio en métodos separados
     prices_A = list(map(lambda place: zoneA(place), list(filter(lambda x: x['zona']=='A', DATA))))
     prices_B = list(map(lambda place: zoneB(place), list(filter(lambda x: x['zona']=='B', DATA))))
 
     estate_prices = list(filter(lambda x: x['precio']<=budget, prices_A+prices_B))
     
+    #Finalmente si la lista es distinto de vacío imprime su contenido, caso contrario informa al usuario que no existen inmuebles con el presupuesto dado
     if estate_prices:
         return print(estate_prices)
     else:
--- taxes_tribute.py
+++ taxes_tribute.py
@@ -1,11 +1,13 @@
 def run():
     
+    #Se valida que la edad y los ingresos sean numeros y mayores a 0
     age = input("Enter your age \n")
     assert age.isnumeric(), "Only numeric values greater than 0"
     
     incomes = input("Input your monthly income \n")
     assert incomes.isnumeric(), "Only numeric values greater than 0"
 
+    #Si cumple que es mayor de edad y sus ingresos son mayores o iguales a 1000 deberá tributar, caso contrario no
     if int(age)>17 and int(incomes) >=1000: print("Must Tribute")
     else: print("Must not tribute")
 
